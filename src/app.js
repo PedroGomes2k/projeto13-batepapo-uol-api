bb0dd3c8575fc63dec87370e66fa0c5fab659754
PedroGomes2k <ph.gomes2k@gmail.com.br>
2023-08-16 11:02:44 -0300

    Requisito: Post and Get do participantes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,50 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const participantes = []
+const mensagens = []
+
+app.post('/participants', (req, res) => {
+
+    const { name } = req.body
+
+    if (name === "") {
+        return res.sendStatus(422)
+    } else if (participantes.find(n => n.name === name)) {
+        return res.sendStatus(409)
+    }
+
+    participantes.push({ name })
+
+    res.sendStatus(201)
+
+})
+
+app.get('/participants', (req, res) => {
+
+    if (participantes.length === 0) {
+        return []
+    }
+    console.log(participantes)
+    res.send(participantes)
+})
+
+
+app.post('/messages', (req, res) => {
+
+    const { to, text, type } = req.body
+
+    if (to === "" || text === "" || type !== "private_message" || type !== "message") {
+        return res.sendStatus(422)
+    }
+
+
+    res.sendStatus(201)
+})
+
+app.get('/messages', (req, res) => {
+
+})
 
 
 
